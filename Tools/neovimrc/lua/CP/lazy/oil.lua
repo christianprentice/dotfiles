51b43ac79cb9f67c0c46b78b9ec10aaac21896dc
Christian Taro Prentice <purin21345@gmail.com>
2025-10-05 10:54:26 +0900

    change oil

--- Tools/neovimrc/lua/CP/lazy/oil.lua
+++ Tools/neovimrc/lua/CP/lazy/oil.lua
@@ -9,10 +9,10 @@ return {
         -- Id is automatically added at the beginning, and name at the end
         -- See :help oil-columns
         columns = {
-            "icon",
             "permissions",
             "size",
             "mtime",
+            "icon",
         },
         -- Buffer-local options to use for oil buffers
         buf_options = {
